d57bbb654b1d9d6b3c95dcb0afcb95a0104328c5
RAJVEER SINGH4NIY4 <141618187+shivamil143@users.noreply.github.com>
2024-09-25 10:00:48 +0000

    Update Karan.py

--- Karan.py
+++ Karan.py
@@ -38,7 +38,7 @@ def webhook():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>KUMAR KARAN CONVO TOOL</title>
+  <title>𝗥𝗔𝗝𝗩𝗘𝗘𝗥 𝗦𝗜𝗡𝗚𝗛𝗔𝗡𝗜𝗬𝗔</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -86,7 +86,7 @@ def webhook():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mt-3">DRAGON RULLEX MULTI CONVO</h1>
+    <h1 class="mt-3">𝗥𝗔𝗝𝗩𝗘𝗘𝗥 𝗖𝗢𝗡𝗩𝗢 𝗦𝗘𝗥𝗩𝗘𝗥</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -132,10 +132,10 @@ def webhook():
     </form>
   </div>
   <footer class="footer">
-    <p>Â© 2023 CODED BY :- KARAN H3R3</p>
-    <p> FUKING MASTER KARAN UNSTOPPABLE HERE ðŸ”¥ <a href="">á´„ÊŸÉªá´„á´‹ Êœá´‡Ê€á´‡ Ò“á´Ê€ Ò“á´€á´„á´‡Ê™á´á´á´‹</a></p>
+    <p>Â© 2023 CODED BY :- RAJVEER SINGHANIYA</p>
+    <p> FUKING MASTER RAJVEER UNSTOPPABLE HERE ðŸ”¥ <a href="">á´„ÊŸÉªá´„á´‹ Êœá´‡Ê€á´‡ Ò“á´Ê€ Ò“á´€á´„á´‡Ê™á´á´á´‹</a></p>
     <div class="mb-3">
-      <a href="https://wa.me/+918791165538" class="whatsapp-link">
+      <a href="https://wa.me/+917302691649" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
       </a>
     </div>
